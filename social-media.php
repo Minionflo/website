f94bec21c7236a0539f8df9f196bcf5390b90edd
Minionflo <github@minionflo.net>
2021-06-13 16:58:17 +0200

    Changed size (very much work)

--- social-media.php
+++ social-media.php
@@ -42,24 +42,24 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         <li><?php echo $_navbar_log; ?></li>
     </ul>
     <h1 align=center class="title">Social Media</h1>
-    <p align=center id="discord-pos"><a id="discord" href="http://discord.minionflo.net" target="_blank">Discord</a></p>
-    <p align=center id="youtube-pos"><a id="youtube" href="http://youtube.minionflo.net" target="_blank">Youtube</a></p>
-    <p align=center id="reddit-pos"><a id="reddit" href="http://reddit.minionflo.net" target="_blank">Reddit</a></p>
-    <p align=center id="spotify-pos"><a id="spotify" href="http://spotify.minionflo.net" target="_blank">Spotify</a></p>
-    <p align=center id="twitter-pos"><a id="twitter" href="http://twitter.minionflo.net" target="_blank">Twitter</a></p>
-    <p align=center id="instagram-pos"><a id="instagram" href="http://instagram.minionflo.net"
-            target="_blank">Instagram</a></p>
-    <p align=center id="twitch-pos"><a id="twitch" href="http://twitch.minionflo.net" target="_blank">Twitch</a></p>
-    <p align=center id="tiktok-pos"><a id="tiktok" href="http://tiktok.minionflo.net" target="_blank">Tiktok</a></p>
-    <p align=center id="steam-pos"><a id="steam" href="http://steam.minionflo.net" target="_blank">Steam</a></p>
-    <p align=center id="github-pos"><a id="github" href="http://github.minionflo.net" target="_blank">Github</a></p>
-    <p align=center id="facebook-pos"><a id="facebook" href="http://facebook.minionflo.net" target="_blank">Facebook</a>
-    </p>
-    <p align=center id="tellonym-pos"><a id="tellonym" href="http://tellonym.minionflo.net" target="_blank">Tellonym</a>
-    </p>
-    <p align=center id="snapchat-pos"><a id="snapchat" href="http://snapchat.minionflo.net" target="_blank">Snapchat</a>
-    </p>
-    <p align=center id="email-pos"><a id="email" href="mailto:public@minionflo.net" target="_blank">E-Mail</a></p>
+    <div align=center>
+        <ul class="ul">
+            <li><a class="ul-li-a" href="http://discord.minionflo.net" target="_blank">Discord</a></li>
+            <li><a class="ul-li-a" href="http://youtube.minionflo.net" target="_blank">Youtube</a></li>
+            <li><a class="ul-li-a" href="http://reddit.minionflo.net" target="_blank">Reddit</a></li>
+            <li><a class="ul-li-a" href="http://spotify.minionflo.net" target="_blank">Spotify</a></li>
+            <li><a class="ul-li-a" href="http://twitter.minionflo.net" target="_blank">Twitter</a></li>
+            <li><a class="ul-li-a" href="http://instagram.minionflo.net" target="_blank">Instagram</a></li>
+            <li><a class="ul-li-a" href="http://twitch.minionflo.net" target="_blank">Twitch</a></li>
+            <li><a class="ul-li-a" href="http://tiktok.minionflo.net" target="_blank">Tiktok</a></li>
+            <li><a class="ul-li-a" href="http://steam.minionflo.net" target="_blank">Steam</a></li>
+            <li><a class="ul-li-a" href="http://github.minionflo.net" target="_blank">Github</a></li>
+            <li><a class="ul-li-a" href="http://facebook.minionflo.net" target="_blank">Facebook</a></li>
+            <li><a class="ul-li-a" href="http://tellonym.minionflo.net" target="_blank">Tellonym</a></li>
+            <li><a class="ul-li-a" href="http://snapchat.minionflo.net" target="_blank">Snapchat</a></li>
+            <li><a class="ul-li-a" href="mailto:public@minionflo.net" target="_blank">E-Mail</a></li>
+        </ul>
+    </div>
 </body>
 
 </html
